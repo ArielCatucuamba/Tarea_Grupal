b213e450219b07dd6c583111d5534b71967628f1
OMEN <davidcatudiaz22@gmail.com>
2024-05-29 20:17:22 -0500

    Herencia grupal

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    //Atributos
+    String especie;
+    String habitat;
+    double tamaño;
+    //metodo constructor
+    public Animal(String especie,String habitat,double tamaño){
+        this.especie=especie;
+        this.habitat=habitat;
+        this.tamaño=tamaño;
+    }
+    //metodo
+    public void Mostrar(){
+        System.out.println("*********Datos del animal*********");
+        System.out.println("Especie: "+especie);
+        System.out.println("Habitata: "+habitat);
+        System.out.println("Tamaño: "+tamaño +" cm");
+    }
+}
